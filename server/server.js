d03317e27fbe470df07498fc9582d0fc69d23a2d
IZERE <izerefaifelix@gmail.com>
2021-02-04 16:48:22 +0200

    Ignore unecessary folder

--- server/server.js
+++ server/server.js
@@ -1,21 +1,7 @@
 import dotenv from "dotenv";
 import app from "./index";
 import mongoose from "mongoose";
-// const config = require('./config/database')
-// Make connection to DB 
-// database:'mongodb://localhost:27017/simpledb'
-// mongoose.connect(config.database,{useNewUrlParser:true})
-// let db = mongoose.connection;
-// //Check for DB connection? 
-
-// // Check for DB errors
-// db.once('open',()=>console.log('mongoDB ok'))
-// db.on('err', () => console.log(err));
-
-
 dotenv.config({ path: "./server/config.env" });
-
-
 const port = process.env.PORT;
 
 const DB = process.env.DATABASE;
